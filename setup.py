d85e5836a9a59da58df8dd62ba6d084e56d90136
Josue Kouka <jkouka@scaleway.com>
2019-06-20 22:50:55 +0200

    remove useless supported version of python

--- setup.py
+++ setup.py
@@ -43,8 +43,7 @@ setup(
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3',
         'Development Status :: 5 - Production/Stable',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Environment :: Console',
